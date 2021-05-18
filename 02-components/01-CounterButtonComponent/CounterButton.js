0bc1808d6d5990aac5cfe6c14e972f163554d11c
javascriptru <67475495+jsru-1@users.noreply.github.com>
2021-05-18 18:13:46 +0300

    Added teacher solution for task: Компонент CounterButton

---
+++ 02-components/01-CounterButtonComponent/CounterButton.js
@@ -0,0 +1,26 @@
+export default {
+  name: 'CounterButton',
+
+  // Теперь текущее значение счётчика приходит от родителя через входной параметр
+  props: {
+    count: {
+      type: Number,
+      default: 0,
+    },
+  },
+
+  // Описываем модель компонента на параметр count с событием increment
+  model: {
+    prop: 'count',
+    event: 'increment',
+  },
+
+  methods: {
+    increment() {
+      // При клике на кнопку порождаем событие и отправляем новое значение
+      this.$emit('increment', this.count + 1);
+    },
+  },
+
+  template: '<button type="button" @click="increment">{{ count }}</button>',
+};
