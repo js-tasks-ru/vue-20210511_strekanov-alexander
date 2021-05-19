f40d58c8ac6f54d4c6ae0cc2d94ffd729bbb86a4
javascriptru <67475495+jsru-1@users.noreply.github.com>
2021-05-19 16:29:05 +0300

    Added task MeetupAgenda

---
+++ 02-components/05-MeetupAgenda/MeetupAgenda.js
@@ -0,0 +1,13 @@
+// import { MeetupAgendaItem } from './MeetupAgendaItem.js';
+
+export default {
+  name: 'MeetupAgenda',
+
+  template: `
+    <div class="meetup-agenda">
+      <div class="meetup-agenda__item"></div>
+      <div class="meetup-agenda__item"></div>
+      <!-- ... -->
+      <div class="meetup-agenda__item"></div>
+    </div>`,
+};
