919fac6c27409e8c296c30c948016cbf50fc5ade
javascriptru <67475495+jsru-1@users.noreply.github.com>
2021-05-19 15:09:45 +0300

    Added teacher solution for task: MeetupCover

---
+++ 02-components/03-MeetupCover/MeetupCover.js
@@ -0,0 +1,13 @@
+export default {
+  name: 'MeetupCover',
+
+  props: {
+    title: String,
+    link: String,
+  },
+
+  template: `
+    <div class="meetup-cover" :style="link && '--bg-url: url(\\'' + link + '\\')'">
+        <h1 class="meetup-cover__title">{{ title }}</h1>
+    </div>`,
+};
